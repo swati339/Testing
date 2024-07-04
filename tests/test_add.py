8861a926b63c5e5c02c70fc7b0070e68e1d019e4
Aesha <swatidutta993@gmail.com>
2024-07-04 21:58:43 +0545

    added test

--- tests/test_add.py
+++ tests/test_add.py
@@ -1,6 +1,18 @@
 import unittest
 from add_numbers import add
 
+import sys
+import os
+
+# Add the parent directory of the current file to sys.path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+
+
+assert add(1, 2) == 3
+print("Assertion passed!")
+
+
 class TestAddFunction(unittest.TestCase):
     def test_add(self):
         self.assertEqual(add(2, 3), 5)
@@ -8,8 +20,9 @@ class TestAddFunction(unittest.TestCase):
         self.assertEqual(add(0, 0), 0)
 
 
-assert add(1, 2) == 3
-print("Assertion passed!")
+
 
 if __name__ == '__main__':
+    
+
     unittest.main()

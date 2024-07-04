e5e8105fbf87dd69675d4a82edd1adcc0f1ab85c
Aesha <swatidutta993@gmail.com>
2024-07-04 22:55:19 +0545

    subtract function added

--- tests/test_add.py
+++ tests/test_add.py
@@ -5,7 +5,7 @@ import sys
 import os
 
 # Add the parent directory of the current file to sys.path
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+# sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 
 

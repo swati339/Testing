ed893b51614b4ba667b271ec4f19928405dc355d
Aesha <swatidutta993@gmail.com>
2024-07-05 21:51:53 +0545

    Mark decorator used

--- tests/test_add.py
+++ tests/test_add.py
@@ -7,13 +7,14 @@ import os
 
 # Add the parent directory of the current file to sys.path
 # sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
+@pytest.mark.addition
 def test_add():
     assert add(1, 2) == 3
     assert add(2, 3) == 5
     assert add(-1, 1) == 0
     assert add(0, 0) == 0
 
+@pytest.mark.subtraction
 def test_subtract():
     assert subtract(5, 3) == 2
     assert subtract(3, 5) == -2
